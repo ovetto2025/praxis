6fac695c1e10ad63229ae2cd89a28508d4495523
ovetto1997 <spartanluca@gmail.com>
2025-11-11 00:42:14 +0100

    added new file e folders e change location of files, implementazione di firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:praxis/core/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'features/authentication/presentation/screens/registration_page.dart';
 
-import 'features/presentation/screens/registration_page.dart';
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Praxis());
 }
 
