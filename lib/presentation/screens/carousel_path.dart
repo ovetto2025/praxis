c84767417d650bff1164c405b1bf9e2dc172fb53
MauroMolino <Mauro.Molino@edu-its.it>
2025-11-23 10:49:11 +0100

    Carousel working

--- lib/presentation/screens/carousel_path.dart
+++ lib/presentation/screens/carousel_path.dart
@@ -1,45 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class CarouselPath extends StatelessWidget {
-  static const String routeName = '/carousel';
+class CarouselPath extends StatefulWidget {
+  static const String routeName = "/carousel";
   const CarouselPath({super.key});
+
+  @override
+  State<CarouselPath> createState() => CarouselPathState();
+}
+
+class CarouselPathState extends State<CarouselPath> {
+  int selectedContent = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Scegli il tuo percorso")),
-      body: Center(
-        child: CarouselSlider(
-          options: CarouselOptions(
-            aspectRatio: 1,
-            enlargeCenterPage: false,
-            enableInfiniteScroll: true,
+      body: Column(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              aspectRatio: 1.75,
+              enlargeCenterPage: true,
+              enableInfiniteScroll: true,
+              viewportFraction: 0.4,
+            ),
+            items:
+                [
+                  "lib/core/assets/images/Percorso Culturalle.png",
+                  "lib/core/assets/images/Percorso Famiglia.png",
+                  "lib/core/assets/images/Percorso Sport.png",
+                  "lib/core/assets/images/Percorso Completo.jpg",
+                ].map((item) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Image.asset(item, fit: BoxFit.contain),
+                      );
+                    },
+                  );
+                }).toList(),
           ),
-          items:
-              [
-                'lib/core/assets/images/Percorso Culturalle.png',
-                'lib/core/assets/images/Percorso Famiglia.png',
-                'lib/core/assets/images/Percorso Sport.png',
-                null, // Placeholder for the fourth image
-              ].map((item) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: item != null
-                          ? Image.asset(item, fit: BoxFit.cover)
-                          : const Center(child: Text('Placeholder')),
-                    );
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedContent = 0;
+                    });
                   },
-                );
-              }).toList(),
-        ),
+                  style: FilledButton.styleFrom(
+                    backgroundColor: selectedContent == 0
+                        ? Colors.red
+                        : Colors.white,
+                  ),
+                  child: const Text("Dettagli percorso"),
+                ),
+                FilledButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedContent = 1;
+                    });
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: selectedContent == 1
+                        ? Colors.red
+                        : Colors.white,
+                  ),
+                  child: const Text("Elenco luoghi"),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: selectedContent == 0
+                ? Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints.expand(),
+                      child: Text('ciao, bella ciao'),
+                    ),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text('ciao, brutta ciao')],
+                    ),
+                  ),
+          ),
+        ],
       ),
     );
   }
